ac33937b1f81104d4ed776994c3fe5de2547dbcd
Khuraijam Gunindro <65593707+KhGunindro@users.noreply.github.com>
2025-02-22 15:04:12 +0530

    added page navigatiom

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from 'react'
+import { Stack } from 'expo-router'
+import { Image } from 'react-native'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+const Layout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <Stack screenOptions={{headerShadowVisible: true}}>
+        <Stack.Screen name="(tabs)" options={{
+            headerTitle: 'Veritoken',
+            headerStyle: { backgroundColor: '#4177ca' },
+            headerTitleStyle: { fontFamily: 'Sans Serif', fontSize: 20, color: 'white' },
+            headerRight: () => (
+                <Image
+                    source={require('../assets/images/logo.png')}
+                    style={{ width: 50, height: 45 }}
+                />
+            ),
+        }} />
+        <Stack.Screen name='QrGenerator' options={{headerTitle: 'QR code generator'}}/>
+    </Stack>
+  )
 }
+
+export default Layout
